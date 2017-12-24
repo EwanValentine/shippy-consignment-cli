3694a2c380674a041f0bbf18b66438b2b095030d
Ewan Valentine <ewan.valentine89@gmail.com>
2017-12-24 22:07:26 +0000

    init

---
+++ Makefile
@@ -0,0 +1,5 @@
+build:
+	docker build -t consignment-cli .
+
+run:
+	docker run -e MICRO_REGISTRY=mdns consignment-cli
---
+++ cli.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	pb "github.com/EwanValentine/shippy/consignment-service/proto/consignment"
+	microclient "github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/cmd"
+	"github.com/micro/go-micro/metadata"
+	"golang.org/x/net/context"
+)
+
+const (
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(file string) (*pb.Consignment, error) {
+	var consignment *pb.Consignment
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &consignment)
+	return consignment, err
+}
+
+func main() {
+
+	cmd.Init()
+
+	// Create new greeter client
+	client := pb.NewShippingServiceClient("go.micro.srv.consignment", microclient.DefaultClient)
+
+	// Contact the server and print out its response.
+	file := defaultFilename
+	var token string
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+		token = os.Args[2]
+	}
+
+	consignment, err := parseFile(file)
+
+	if err != nil {
+		log.Fatalf("Could not parse file: %v", err)
+	}
+
+	r, err := client.CreateConsignment(context.TODO(), consignment)
+	if err != nil {
+		log.Fatalf("Could not create: %v", err)
+	}
+	log.Printf("Created: %t", r.Created)
+
+	ctx := metadata.NewContext(context.Background(), map[string]string{
+		"token": token,
+	})
+
+	getAll, err := client.GetConsignments(ctx, &pb.GetRequest{})
+	if err != nil {
+		log.Fatalf("Could not list consignments: %v", err)
+	}
+	for _, v := range getAll.Consignments {
+		log.Println(v)
+	}
+}
